8ee94a269b47bb08403310c592316f93e44cfa06
Mafika <rudolphmafika@gmail.com>
2025-11-19 22:25:42 +0200

    User registration

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
+
 const app = express();
 
-app.get('/', (req, res) => res.send('API running....'));
+//Connect to database
+connectDB()
+
+// Init middleware
+app.use(express.json({extebded: false}));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
